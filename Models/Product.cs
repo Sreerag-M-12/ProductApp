78ff59a8e66ee70789333de4e9eb663a5bf54542
Sreerag Rajesh Babu <Sreerag.Babu@asl.mum.sst>
2024-07-12 20:02:43 +0530

    innit

---
+++ Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ProductApp.Models
+{
+    internal class Product
+    {
+        private int _id;
+        private string _productName;
+        private double _price;
+        private double _discountPercent;
+
+        public int Id{    set { _id = value; } get { return _id; }       }
+        public string ProductName { set { _productName = value; } get { return _productName; } }
+
+        public double Price { set { _price = value; } get { return _price; } }
+
+        public double DiscountPercent { set { _discountPercent = value; } get { return _discountPercent; } }
+
+        public double CalculateNewPrice()
+        {
+              return _price - (_price * (_discountPercent/100));  
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using ProductApp.Models;
+
+namespace ProductApp
+{
+    internal class Program
+    {
+        static void PrintDetails(Product p)
+        {
+            Console.WriteLine($"id: {p.Id} \n" +
+                $"name: {p.ProductName} \n" +
+                $"price: {p.Price}  \n" +
+                $"discount: {p.DiscountPercent} \n"+
+                $"new Price: {p.CalculateNewPrice()}");
+        }
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.ProductName = "Television";
+            product1.Id = 100;
+            product1.Price = 15000;
+            product1.DiscountPercent = 20;
+
+            PrintDetails(product1);
+
+            Console.WriteLine("+++++++++++++++++++++");
+
+            Product product2 = new Product();
+            product2.ProductName = "WashingMachine";
+            product2.Id = 101;
+            product2.Price = 10000;
+            product2.DiscountPercent = 10;
+
+            PrintDetails(product2);
+
+
+
+            Console.WriteLine("+++++++++++++++++++++");
+
+        }
+    }
+}
